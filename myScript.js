fad6e99f99714863c17293703d968272da42ad0f
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-09-29 15:59:46 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -13,6 +13,17 @@ console.log(sname + semail + snumber + spass + srepass);
 
 if(sname.length==0 || semail.length==0 || snumber.length==0 || spass.length==0 || srepass.length==0){
   alert("Please fill all the fields");
+
+    if(snumber.length<10 || snumber.length>11){
+      alert("Phone number should be of length 10");
+    }
+
+    if(spass===srepass){
+      alert("Password and Repassword do not match");
+    }
+
+
+
 }
 
 }

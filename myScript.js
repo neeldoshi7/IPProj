fdfe357bd80acbba9063f1713e34a1e469e006ba
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-09-29 15:46:41 +0530

    formvalid

--- myScript.js
+++ myScript.js
@@ -2,4 +2,15 @@ console.log("Hello from my javascript");
 
 function myCheck(){
   console.log("Hello on click");
+
+sname = document.querySelector('#fname').value();
+semail = document.querySelector('#femail').value();
+snumber = document.querySelector('#pnumber').value();
+spass = document.querySelector('#fpass');
+srepass = document.querySelector('#frepass');
+
+if(sname.length==0 || semail.length==0 || snumber.length==0 || spass.length==0 || srepass.length==0){
+  alert("Please fill all the fields");
+}
+
 }

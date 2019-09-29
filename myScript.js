aab7dc08802002e94b894a5a75f69e1c242635c2
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-09-29 14:52:06 +0530

    changes

--- myScript.js
+++ myScript.js
@@ -1,1 +1,5 @@
 console.log("Hello from my javascript");
+
+function myCheck(){
+  console.log("Hello on click");
+}
--- submitform.js
+++ submitform.js
@@ -1,4 +1,1 @@
-<<<<<<< HEAD
-=======
 console.log("Hello from submit form");
->>>>>>> 74f13ff50fbedc04fe8217283c540ffa6849a7b7

31cb9d5606a0da06ac9ed670017b2fb95ccf31ca
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-10-01 10:33:29 +0530

    updating form handling

---
+++ signup.php
@@ -0,0 +1,20 @@
+<?php
+
+require "connection.php";
+
+$name = $_POST["sname"];
+$email = $_POST["semail"];
+$pno = $_POST["spno"];
+$pass = $_POST["spass"];
+
+$mysqlquery = "insert into user(name , email , phoneno , password ) values ('{$name}','{$email}','{$pno}','{$pass}')";
+
+if($conn->query($mysqlquery) === TRUE){
+  echo "Insert successful";
+}
+else {
+  echo "Error : ". $mysqlquery . "<br>" . $conn->error;
+}
+$conn->close();
+
+ ?>

73b8642864bfce4b51f4a36418fd258895285b7c
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-10-03 09:51:21 +0530

    chngs

--- login.php
+++ login.php
@@ -1,6 +1,26 @@
 <?php
 require "connection.php";
 
+$email = $_POST["lemail"];
+$pass = $_POST["lpass"];
 
+$mysqlquery2 = "select password from user where email like $email";
+
+$result = $conn->query($mysqlquery2);
+
+if($result -> num_rows === 1){
+  $row = $result -> fetch_assoc();
+  if($row["password"] != $pass){
+    echo '<script language="javascript">';
+    echo 'alert("Incorrect password")';
+    echo '</script>';
+  }
+
+}
+else {
+  echo '<script language="javascript">';
+  echo 'alert("User doest not exist")';
+  echo '</script>';
+}
 
  ?>

d07da647a8b1f2658219f890d5978ebe9a8b98f8
jhanvishah <jhanvishah26@gmail.com>
2019-10-06 20:14:26 +0530

    Update selltable.php

--- selltable.php
+++ selltable.php
@@ -6,7 +6,7 @@ $p_description = $_POST["prod_desc"];
 $amount = $_POST["prod_amount"];
 $image = $_FILES["prod_img"]["name"];
 echo "inage" . $image;
-$img = file_get_contents($image);
+$img = addslashes(file_get_contents($image));
 
 $mysqlquery4 = "insert into products(p_name , p_description , amount , image ) values ('{$p_name}','{$p_description}','{$amount}','{$img}')";
 

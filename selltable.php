6ab75d413d9bf6f10bb8ae1a149815277c8d4a9b
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-10-06 12:20:07 +0530

    Update selltable.php

--- selltable.php
+++ selltable.php
@@ -4,9 +4,10 @@ require "connection.php";
 $p_name = $_POST["prod_name"];
 $p_description = $_POST["prod_desc"];
 $amount = $_POST["prod_amount"];
-$image = $_POST["prod_img"];
+$image = $_FILES['image']['tmp_name'];
+    $img = file_get_contents($image);;
 
-$mysqlquery4 = "insert into product(p_name , p_description , amount , image ) values ('{$p_name}','{$p_description}','{$amount}','{$image}')";
+$mysqlquery4 = "insert into product(p_name , p_description , amount , image ) values ('{$p_name}','{$p_description}','{$amount}','{$img}')";
 
 if($conn->query($mysqlquery4) === TRUE){
   echo "Insert successful";
@@ -15,7 +16,7 @@ if($conn->query($mysqlquery4) === TRUE){
 }
 else {
   echo "Error : ". $mysqlquery4 . "<br>" . $conn->error;
-  
+
 }
 $conn->close();
 

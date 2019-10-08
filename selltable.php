833bc47fdf2ba13b0644a5f099c5db0e89c69342
jhanvishah <jhanvishah26@gmail.com>
2019-10-08 17:44:45 +0530

    Update selltable.php

--- selltable.php
+++ selltable.php
@@ -8,11 +8,10 @@ $image = $_FILES['prod_img']['name'];
 echo "inage" . $image;
 $img = addslashes(file_get_contents($image));
 
-$mysqlquery4 = "insert into products(p_name , p_description , amount , image ) values ('{$p_name}','{$p_description}','{$amount}','{$img}')";
+$mysqlquery4 = "insert into products(p_name , p_description , seller, buyer, amount , image , sold ) values ('{$p_name}','{$p_description}', "aa" , "bb", '{$amount}','{$img}', true)";
 
 if($conn->query($mysqlquery4) === TRUE){
   echo "Insert successful";
-
 }
 else {
   echo "Error : ". $mysqlquery4 . "<br>" . $conn->error;

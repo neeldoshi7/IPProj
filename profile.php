2f866761e9c004f3eaa6f89242ba4c752294ad78
jhanvishah <jhanvishah26@gmail.com>
2019-10-09 15:41:06 +0530

    jjk

--- profile.php
+++ profile.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
+// session_start();
 echo "<h3> PHP List All Session Variables</h3>";
     foreach ($_SESSION as $key=>$val)
     echo $key." ".$val."<br/>";
-require "connection.php";
+
 require "connection.php";
 
 $email = $_SESSION["email"];

90723f635a275918fc03a7faf6a6df15631aefb5
jhanvishah <jhanvishah26@gmail.com>
2019-10-09 15:26:25 +0530

    Update selltable.php

--- selltable.php
+++ selltable.php
@@ -1,5 +1,8 @@
 <?php
 session_start();
+echo "<h3> PHP List All Session Variables</h3>";
+    foreach ($_SESSION as $key=>$val)
+    echo $key." ".$val."<br/>";
 require "connection.php";
 
 $email = $_SESSION["email"];

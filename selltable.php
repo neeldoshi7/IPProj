309bcbb25903c71aa160f309b2fad0e05588731b
jhanvishah <jhanvishah26@gmail.com>
2019-10-09 10:46:23 +0530

    Update selltable.php

--- selltable.php
+++ selltable.php
@@ -9,7 +9,7 @@ $image = $_FILES['prod_img']['tmp_name'];
 // echo "image " . $image;
 $img = addslashes(file_get_contents($image));
 
-$mysqlquery4 = "insert into products(p_name , p_description , seller , buyer , amount , image ) values ('{$p_name}','{$p_description}', '{$email}', " " , '{$amount}','{$img}')";
+$mysqlquery4 = "insert into products(p_name , p_description , seller , buyer , amount , image ) values ('{$p_name}','{$p_description}', '{$email}', ' ', '{$amount}','{$img}')";
 
 if($conn->query($mysqlquery4) === TRUE){
   echo "Insert successful";

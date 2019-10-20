0dc5cefc10f23a229f5b95eb171d55ad070eaf0e
jhanvishah <jhanvishah26@gmail.com>
2019-10-20 11:05:37 +0530

    Update sellitem.php

--- sellitem.php
+++ sellitem.php
@@ -1,6 +1,8 @@
 <?php
 
 session_start();
+if (isset($_SESSION['admin']))
+    echo ' all html code ';
 echo "<h3> PHP List All Session Variables</h3>";
     foreach ($_SESSION as $key=>$val)
     echo $key." ".$val."<br/>";

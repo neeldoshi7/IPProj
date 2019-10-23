c7845a319627f0818d05db21ce931996e3bf4a3f
neeldoshi7 <47111448+neeldoshi7@users.noreply.github.com>
2019-10-23 22:05:03 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -114,7 +114,7 @@ echo $_SESSION["name"];
 
 <div><form action="buy.php"><input type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-shopping-cart"></span>BUY</form></div>
 
-<div><form action="sellitem.php" method="post"><input type="submit" class="btn btn-primary" >SELL</form></div>
+<div><form action="sellitem.php" method="post"><input type="submit" class="btn btn-primary">SELL</form></div>
 
 <div>
   <h3>Products Sold :

61ae7fd3bfba927640c21d937058ce4d70ea080d
jhanvishah <jhanvishah26@gmail.com>
2019-10-31 11:46:48 +0530

    Update listview.php

--- listview.php
+++ listview.php
@@ -202,7 +202,34 @@ for(var s of sold){
     var c = sss[0].childNodes;
     console.log(c[1].innerText);
     //console.log(sss);
+    document.cookie = "pid=" + c[1].innerText
+    console.log(document.cookie)
+    <?php
+    $_SESSION['pid'] = $_COOKIE['pid'];
+    $pid = $_SESSION['pid'];
+    echo "IN SELLLLLL";
+    $q = "select * from product_bidding where p_id like '{$pid}'";
+    $r = $conn->query($q);
+    if($r->num_rows == 1){
+      $row = $r->fetch_assoc();
+      $buyer_email = $row['bidder_email'];
+      $seller_email = $row['seller_email'];
+      $amount = $row['amount'];
+    }
 
+
+
+    $q1 = "update product set sold = 1 where p_id like '{$pid}'";
+    $q2 = "delete from product_bidding where p_id like '{$pid}'";
+    $q3 = "insert into product_bought (p_id, email, amount) values ('{$pid}', '{$buyer_email}', '{$amount}')";
+    $q4 = "delete from product_selling where p_id likr '{$pid}'";
+    $q5 = "insert into product_sold (p_id, email, amount) values ('{$pid}', {'$seller_email'}, '{$amount}')";
+    $r1 = $conn->query($q1);
+    $r2 = $conn->query($q2);
+    $r3 = $conn->query($q3);
+    $r4 = $conn->query($q4);
+    $r5 = $conn->query($q5);
+    ?>
     //SO basically c[1].innerText is your pid variable
 
       // console.log($('tr').eq(s));
